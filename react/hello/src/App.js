d585630bc8ce9b0b13fb5eedb87e785923cf6507
Thoritie <thohirh9@gmail.com>
2018-04-22 07:04:40 +0700

    docker and react page showcase  :whale:

--- react/hello/src/App.js
+++ react/hello/src/App.js
@@ -1,18 +1,80 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './index.scss';
+import './bulma.css';
+import './hello.css';
+import './hello.js';
+
+
+
+var divStyle = {
+  width: 500
+};
+
+var tech = {
+  paddingTop: 20,
+  width: 100
+};
+
+var tech1 = {
+  width: 50
+};
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <section className="hero is-info is-large">
+          <div className="hero-head">
+              <header className="nav">
+                <div className="container">
+                    
+                    
+                    
+                </div>
+              </header>
+          </div>
+          <div className="hero-body">
+   
+              <div className="container has-text-centered">
+                {/* <h1 className="title">
+                    Title
+                </h1>
+                <h2 className="subtitle">
+                   Subtitle
+                </h2> */}
+
+                  <div class="columns is-mobile">
+                    <div class="column"><img src="/react.png" style={tech1}/> </div>
+                    <div class="column"><img src="/bulma.png" style={tech}/> </div>
+                    
+                  </div>
+      
+                <link href="https://fonts.googleapis.com/css?family=Black+Han+Sans|Fredoka+One|Press+Start+2P|Roboto+Mono|Martel" rel="stylesheet"/>
+
+                {/* <link href="https://fonts.googleapis.com/css?family=Raleway:200,100,400" rel="stylesheet" type="text/css" /> */}
+                  <h1>Docker  is  &nbsp;
+                    <span
+                      class="txt-rotate"
+                      data-period="2000"
+                      data-rotate='[ "nerdy.", "simple.", "fantastic.", "pretty.", "fun!" ]'></span>
+                  </h1>
+                  {/* <h3>React  &lt;and&gt; Docker.</h3> */}
+                 
+                  <br/>
+                  <br/>
+                  
+                
+
+                                  <img src="/dockerSe.png" style={divStyle}/> 
+
+
+              </div>
+          </div>
+          
+        </section>   
       </div>
     );
   }
